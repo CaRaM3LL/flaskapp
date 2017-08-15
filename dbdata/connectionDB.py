3fe638cc275dd2099edbac81c4b1d2e20cec828a
husse <caramelizatul@yahoo.com>
2017-08-15 18:34:01 +0300

    add db settings

--- dbdata/connectionDB.py
+++ dbdata/connectionDB.py
@@ -1,11 +1,21 @@
 import MySQLdb
 import MySQLdb.cursors
+import config
+
+if config.test:
+    host = "localhost"
+    user = "root"
+    passwd = ""
+    db = "site database"
+else:
+    host = "Husse6.mysql.pythonanywhere-services.com"
+    user = "Husse6"
+    passwd = "Steaua21"
+    db = "Husse6$site_database"
+    
 
 def connection():
-    conn = MySQLdb.connect(host = "localhost",
-                           user = "root",
-                           passwd = "",
-                           db = "site database",
+    conn = MySQLdb.connect(host, user, passwd, db,
                            cursorclass=MySQLdb.cursors.DictCursor)
     cur = conn.cursor()
     return cur, conn
